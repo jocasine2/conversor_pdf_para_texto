6d747df63ad39e8f2fbaa617cd57f329203240e3
Joaquim Campelo da Silva Neto <jcsnetcc@gmail.com>
2022-03-19 14:18:56 -0300

    🚧 Cria rota para controller via api

---
+++ app/Http/Controllers/api/DogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DogController extends Controller{
+    
+    public function index(){
+        echo 'se liga truta';
+    }
+
+    public function create(){
+        //
+    }
+
+    public function store(Request $request){
+        //
+    }
+
+    public function show($id){
+        //
+    }
+
+    public function edit($id){
+        //
+    }
+
+    public function update(Request $request, $id){
+        //
+    }
+
+    public function destroy($id){
+        //
+    }
+}
